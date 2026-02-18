4721f6f7c7ddfaa0cc7d6c9deb161f53685beb60
organizedChaos <bporter@csr.utexas.edu>
2026-02-17 21:32:08 -0600

    Adding in some post-init messages and error checking in app.js

--- app.js
+++ app.js
@@ -704,4 +704,14 @@ function showNotification(message, type) {
     }, 3000);
 }
 
-init().then(r => {});
+/*
+init().then(r => {});*/
+init()
+    .then(() => {
+        console.log('Map initialized successfully');
+        // Do post-initialization tasks
+    })
+    .catch(error => {
+        console.error('Initialization failed:', error);
+        showNotification('Failed to load map', 'error');
+    });
